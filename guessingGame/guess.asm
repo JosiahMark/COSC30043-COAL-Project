60b36fb895a77922c61297a1928af6f622b8e386
CorvusOps <josepholivar124@gmail.com>
2022-02-08 17:56:20 +0800

    Initial commit on guess.asm

---
+++ guessingGame/guess.asm
@@ -0,0 +1,26 @@
+;===================================================;
+; File: guess.asm                                   ;
+; Description:                                      ;
+; Author:                                           ;
+;===================================================;
+; To compile executable (linux ubuntu):             ;
+;                                                   ;
+;                                                   ;
+;===================================================;
+
+; 
+section .text
+
+        global _start 
+
+
+_start:
+
+; Declarations of Data 
+section .data
+
+
+section .bss
+
+
+
