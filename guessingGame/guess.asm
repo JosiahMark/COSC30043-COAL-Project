77abe4398458732f35848e96e80ec2ef5aadc104
Murasakinosora <86734067+Murasakinosora@users.noreply.github.com>
2022-02-11 20:15:32 +0800

    Update guess.asm

--- guessingGame/guess.asm
+++ guessingGame/guess.asm
@@ -36,9 +36,59 @@ _reenter:
 
 ;100 2ppl Josa & Ichu
 _toohigh:
-_toolow:	
+
+    call __write
+    mov ebx, 1 ; Stdout
+    mov ecx, toohigh   ; copies the contents of toohigh onto the ecx
+    mov edx, toohigh_len  ; copies the length of toohigh onto the edx
+    call __syscall  
+
+    jmp _again ;gives the execution control to _again
+
+_toolow:
+    
+    call __write ;
+    mov ebx, 1 ; Stdout
+    mov ecx, toolow  ; copies contents of toolow to the ecx
+    mov edx, toolow_len ; copies the length of toolow to the edx
+    call __syscall 
+
 _again:
+
+    cmp dword [tries], 1  ; compares the number of tries remaining if it is equal to 1
+    jle _lose ;jumps to _lose if the compared variables are equal 
+
+    sub dword [tries], 1;subtracts 1 from the total value of tries
+    
+    jmp _loop ; gives the execution control to _loop
 _lose:
+  
+
+	call __write
+	mov ebx, 1 ; Stdout
+	mov ecx, youlose ;copies the contents of youlose into the ecx
+	mov edx, youlose_len ;copies the length from youlose_len into the edx
+	call __syscall
+
+	mov eax, [randint] ; stores the return value of randint into the eax
+	call __itoa
+
+	mov ecx, eax  ;stores the content of eax to ecx
+	mov edx, ebx  ;stores the content of edx to ebx
+	call __write
+	mov ebx, 1 ; Stdout
+	call __syscall
+
+	call __write
+	mov ebx, 1 ; Stdout
+	mov ecx, youlose2  ;copies the content of youlose2 into the ecx
+	mov edx, youlose2_len ;copies the length from youlose2_len into the edx 
+	call __syscall
+
+	mov ebx, 2 ; Exit code 
+
+	jmp _exit
+
 _convertok:
 _exit:
 	
